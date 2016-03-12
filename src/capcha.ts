855d701d2fb90602b78a097f7d4e4ffea8481e37
pallat <pallat@dtac.co.th>
2016-03-12 14:25:53 +0700

    no if version

--- src/capcha.ts
+++ src/capcha.ts
@@ -2,17 +2,14 @@ class Capcha {
     private leftOperand :Operand;
     private rightOperand :Operand;
     private operator : Operator;
-
-    invertPattern(pattern:number) : number {
-        if (pattern == 1) {
-            return 2
-        }
-        return 1
+    private rightPattern = {
+        1:2,
+        2:1
     }
 
     constructor(public pattern:number,left:number,operator:number, right:number) {
         this.leftOperand = Operand.createOperand(pattern,left);
-        this.rightOperand = Operand.createOperand(this.invertPattern(pattern),right);
+        this.rightOperand = Operand.createOperand(this.rightPattern[pattern],right);
         this.operator = Operator.createOperator(operator);
     }
 
@@ -34,7 +31,15 @@ class Operator {
 }
 
 class Operand {
-    private mapNumber:string[] =  ["", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"];
+    private strNumber:string[] =  ["", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"];
+    private whichNumberPattern = {
+        1: () => {
+            return this.operand.toString();
+        },
+        2: () => {
+            return this.strNumber[this.operand];
+        }
+    }
 
     constructor(public pattern:number, public operand:number){}
 
@@ -43,11 +48,6 @@ class Operand {
     }
 
     toString() :string {
-
-
-        if (this.pattern == 1) {
-            return this.operand.toString();
-        }
-        return this.mapNumber[this.operand];
+        return this.whichNumberPattern[this.pattern]();
     }
 }
