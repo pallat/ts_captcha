7475834c016694db21e14cad5e0c0f83e9a1ce47
pallat <pallat@dtac.co.th>
2016-03-12 14:13:11 +0700

    oop style

--- src/capcha.ts
+++ src/capcha.ts
@@ -1,44 +1,53 @@
 class Capcha {
+    private leftOperand :Operand;
+    private rightOperand :Operand;
+    private operator : Operator;
 
-    constructor(public pattern:number, public left:number, public operator:number, public right:number) {}
-
-    getOperator() :string {
-        let mapOperator = {
-            1: "+",
-            2: "-",
-            3: "*",
-            4: "/"
-        };
-        return mapOperator[this.operator];
+    invertPattern(pattern:number) : number {
+        if (pattern == 1) {
+            return 2
+        }
+        return 1
     }
 
-    getStringNumber(num: number) :string {
-        let mapNumber = {
-            1: "ONE",
-            2: "TWO",
-            3: "THREE",
-            4: "FOUR",
-            5: "FIVE",
-            6: "SIX",
-            7: "SEVEN",
-            8: "EIGHT",
-            9: "NINE"
-        };
-
-        return mapNumber[num]
+    constructor(public pattern:number,left:number,operator:number, right:number) {
+        this.leftOperand = Operand.createOperand(pattern,left);
+        this.rightOperand = Operand.createOperand(this.invertPattern(pattern),right);
+        this.operator = Operator.createOperator(operator);
     }
 
     show() :string {
+        return this.leftOperand.toString() + " " + this.operator.toString() + " " + this.rightOperand.toString();
+    }
+}
 
-        let mapPattern = {
-            1: () => {
-                return this.left.toString() + " " + this.getOperator() + " " + this.getStringNumber(this.right);
-            },
-            2: () => {
-                return this.getStringNumber(this.left) + " " + this.getOperator() + " " + this.right.toString();
-            }
+class Operator {
+    private operators: string[] = ["","+","-","*","/"];
+    constructor(public operator:number) {}
+    static createOperator(operator:number) {
+        return new Operator(operator);
+    }
+
+    toString() :string {
+        return this.operators[this.operator];
+    }
+}
+
+class Operand {
+    private mapNumber:string[] =  ["", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"];
+
+    constructor(public pattern:number, public operand:number){}
+
+    static createOperand(pattern:number, operand:number) {
+        return new Operand(pattern,operand);
+    }
+
+    toString() :string {
+
+
+        if (this.pattern == 1) {
+            return this.operand.toString();
         }
-
-        return mapPattern[this.pattern]();
+        return this.mapNumber[this.operand];
     }
 }
