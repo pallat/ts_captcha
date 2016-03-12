c243d3e7e58f6c57696680026d5ccfb8169208d7
Ball Weera Kasetsin <wkasetsin@gmail.com>
2016-03-12 11:19:18 +0700

    Yod do something

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -4,9 +4,11 @@ class FizzBuzz {
         if ((n % 3) == 0) {
             return "Fizz";
         }
+
         if ((n % 5) == 0) {
             return "Buzz";
         }
+
         return n.toString();
     }
 }
