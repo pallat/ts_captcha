f254f19b1d0e4b926a46f052deabe275c7749d5d
Ball Weera Kasetsin <wkasetsin@gmail.com>
2016-03-12 10:34:22 +0700

    Implement say in fizzbuzz

--- spec/fizzbuzz.spec.ts
+++ spec/fizzbuzz.spec.ts
@@ -2,7 +2,7 @@
 /// <reference path="../src/fizzbuzz.ts" />
 describe("FizzBuzz", () => {
     it("Say 1 should 1", () => {
-        let fizzBuzz = new FizzBuzz();
+        let fizzBuzz:FizzBuzz = new FizzBuzz();
         expect(fizzBuzz.say(1)).toEqual("1");
     });
 });
---
+++ src/fizzbuzz.ts
@@ -0,0 +1,6 @@
+class FizzBuzz {
+    constructor(){}
+    say(n : number): string{
+        return "1";
+    }
+}
